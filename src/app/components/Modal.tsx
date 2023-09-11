d7ac97ddf8e20e1c9fb4f99ed47e284f432fed62
azmyar <azmy.arya.rizaldi@gmail.com>
2023-09-12 02:28:49 +0700

    feat(modal): contributors input

--- src/app/components/Modal.tsx
+++ src/app/components/Modal.tsx
@@ -1,4 +1,6 @@
+import { useCallback, useEffect, useState } from "react";
 import ContributorsCard from "./ContributorsCard";
+import axios from "axios";
 
 interface Contributors {
     url : string,
@@ -8,104 +10,240 @@ interface Contributors {
 
 export default function Modal(props : any) : JSX.Element {
 
+    let endpointFE : string = "";
+    let endpointBE : string = "";
+
     const susunJadwal : Contributors[] = [
         {
             url:"/images/placeholder.png",
-            name:"Tukang Susun Jadwal",
+            name:"Icha",
+            role:"Product Manager"
+        },
+        {
+            url:"/images/placeholder.png",
+            name:"Viona",
+            role:"Product Manager"
+        },
+        {
+            url:"/images/placeholder.png",
+            name:"Fajar",
+            role:"Front-end Engineer"
+        },
+        {
+            url:"/images/placeholder.png",
+            name:"Azmy",
+            role:"Front-end Engineer"
+        },
+        {
+            url:"/images/placeholder.png",
+            name:"Arshad",
             role:"Designer"
         },
         {
             url:"/images/placeholder.png",
-            name:"Tukang Susun Jadwal",
-            role:"Designer"
-        },
-        {
-            url:"/images/placeholder.png",
-            name:"Tukang Susun Jadwal",
-            role:"Designer"
-        },
-        {
-            url:"/images/placeholder.png",
-            name:"Tukang Susun Jadwal",
-            role:"Designer"
-        },
-        {
-            url:"/images/placeholder.png",
-            name:"Tukang Susun Jadwal",
-            role:"Designer"
-        },
-        {
-            url:"/images/placeholder.png",
-            name:"Tukang Susun Jadwal",
-            role:"Designer"
-        },
-        {
-            url:"/images/placeholder.png",
-            name:"Tukang Susun Jadwal",
-            role:"Designer"
+            name:"Davyn Reinhard",
+            role:"Product"
         }
     ]
     
     const bikunTracker : Contributors[] = [
         {
             url:"/images/placeholder.png",
-            name:"Tukang Bikun Tracker",
+            name:"Evan",
+            role:"Product Manager"
+        },
+        {
+            url:"/images/placeholder.png",
+            name:"Qissa",
+            role:"Product Manager"
+        },
+        {
+            url:"/images/placeholder.png",
+            name:"Zuhal",
+            role:"Front-end Engineer"
+        },
+        {
+            url:"/images/placeholder.png",
+            name:"Ricky",
+            role:"Front-end Engineer"
+        },
+        {
+            url:"/images/placeholder.png",
+            name:"Abby",
             role:"Designer"
         },
         {
             url:"/images/placeholder.png",
-            name:"Tukang Bikun Tracker",
-            role:"Designer"
+            name:"Jeremy Alva Pratama",
+            role:"Product"
         },
         {
             url:"/images/placeholder.png",
-            name:"Tukang Bikun Tracker",
-            role:"Designer"
+            name:"Davyn Reinhard",
+            role:"Product"
         },
         {
             url:"/images/placeholder.png",
-            name:"Tukang Bikun Tracker",
-            role:"Designer"
+            name:"Vincent Suryakim",
+            role:"Git Contributor"
         }
     ] 
 
     const ristekLink : Contributors[] = [
         {
             url:"/images/placeholder.png",
-            name:"Tukang Ristek Link",
+            name:"Mira",
+            role:"Product Manager"
+        },
+        {
+            url:"/images/placeholder.png",
+            name:"Indra",
+            role:"Front-end Engineer"
+        },
+        {
+            url:"/images/placeholder.png",
+            name:"Umar",
+            role:"Back-end Engineer"
+        },
+        {
+            url:"/images/placeholder.png",
+            name:"Marcel",
+            role:"Back-end Engineer"
+        },
+        {
+            url:"/images/placeholder.png",
+            name:"Ahmadhi",
+            role:"Back-end Engineer"
+        },
+        {
+            url:"/images/placeholder.png",
+            name:"Arya Adirianto",
             role:"Designer"
         },
         {
             url:"/images/placeholder.png",
-            name:"Tukang Ristek Link",
-            role:"Designer"
-        },
-        {
-            url:"/images/placeholder.png",
-            name:"Tukang Ristek Link",
-            role:"Designer"
+            name:"Jeremy Alva Pratama",
+            role:"Product"
         }
     ] 
 
     const ulasKelas : Contributors[] = [
         {
             url:"/images/placeholder.png",
-            name:"Tukang Ulas Kelas",
+            name:"Dien",
+            role:"Product Manager"
+        },
+        {
+            url:"/images/placeholder.png",
+            name:"Edu",
+            role:"Front-end Engineer"
+        },
+        {
+            url:"/images/placeholder.png",
+            name:"Priyanka",
             role:"Designer"
+        },
+        {
+            url:"/images/placeholder.png",
+            name:"Imo",
+            role:"Designer"
+        },
+        {
+            url:"/images/placeholder.png",
+            name:"Henry Soedibjo",
+            role:"Product"
         }
     ] 
 
-    let app;
+    const [contributors, setContributors] : any = useState([]);
+    const [app, setApp] : any = useState([]);
 
-    if (props.modalApp === 'Susun Jadwal') {
-        app = susunJadwal
-    } else if (props.modalApp === 'Bikun Tracker') {
-        app = bikunTracker
-    } else if (props.modalApp === 'Ristek Link') {
-        app = ristekLink
-    } else {
-        app = ulasKelas
-    }
+    const getGitContributors = async (endpoint : string) =>
+    await axios.get(endpoint);
+
+    const fetchContributors = useCallback(async () => {
+        if (props.modalApp === 'Susun Jadwal') {
+            setApp(susunJadwal)
+            endpointFE = "https://api.github.com/repos/ristekoss/susunjadwal-frontend/contributors"
+            endpointBE = "https://api.github.com/repos/ristekoss/susunjadwal-backend/contributors"
+        } else if (props.modalApp === 'Bikun Tracker') {
+            setApp(bikunTracker)
+        } else if (props.modalApp === 'Ristek Link') {
+            setApp(ristekLink)
+            endpointFE = "https://api.github.com/repos/ristekoss/ristek-link/contributors"
+        } else {
+            setApp(ulasKelas)
+            endpointFE = "https://api.github.com/repos/ristekoss/ulaskelas-frontend/contributors"
+            endpointBE = "https://api.github.com/repos/ristekoss/ulaskelas-backend/contributors"
+        }
+
+        try {
+
+          if (endpointFE){
+            const firstDataContributors = await getGitContributors(endpointFE);
+            if (endpointBE){
+                const secondDataContributors = await getGitContributors(endpointFE);
+                setContributors(MergeContributors(firstDataContributors.data, secondDataContributors.data));
+            } else {
+                setContributors(firstDataContributors.data);
+            }
+          }
+
+        } catch (error) {
+          console.log(error);
+        }
+      }, []);
+
+    const MergeContributors = (contributors : Array<any>, otherContributors : Array<any>) => {
+        if (contributors && otherContributors) {
+            var usernames = new Set(contributors?.map((c) => c.login));
+            usernames.forEach((username) => {
+            if (
+                otherContributors.some(
+                (contributor) => contributor.login === username,
+                )
+            ) {
+                const indexA = contributors.findIndex(
+                (contributor) => contributor.login === username,
+                );
+                const indexB = otherContributors.findIndex(
+                (contributor) => contributor.login === username,
+                );
+
+                contributors[indexA].contributions +=
+                otherContributors[indexB].contributions;
+            }
+            });
+
+            var contributorsMerged = [
+            ...contributors,
+            ...otherContributors?.filter((d) => !usernames.has(d.login)),
+            ]
+            return contributorsMerged;
+        }
+    };
+    
+    useEffect(() => {
+        fetchContributors();
+    }, [fetchContributors]);
+
+    const staticData = app.map((user :any, index : any) =>(
+        <ContributorsCard
+            key={index}
+            name={user.name}
+            url={user.url}
+            role={user.role}
+        />
+    ))
+
+    const gitContributionList = staticData.concat(contributors.map((user : any) => (
+        <ContributorsCard
+            key={user.id}
+            name={user.login}
+            url={user.avatar_url}
+            role="Git Contributor"
+        />
+    )));
 
     return (
         <div className="fade fixed mt-[64px] flex flex-col items-center sm:gap-[20px] md:gap-[40px] gap-[12px] sm:p-[40px] md:p-[60px] p-[20px] w-[90%] sm:max-h-[564px] max-h-[80%] rounded-[20px] bg-white">
@@ -120,11 +258,7 @@ export default function Modal(props : any) : JSX.Element {
                             pt-[8px] pb-[8px]
                             sm:pl-0 pl-[8px]
                             sm:pr-0 pr-[8px]">
-                {app.map((contributors, index) => {
-                    return (
-                    <ContributorsCard url={contributors.url} name={contributors.name} role={contributors.role} key={index}/>
-                    )
-                })}
+                {(contributors != null) && gitContributionList}
             </div>
         </div>
     )
